f99065a8307c3e422ba2ad0912f9fc9608ce6863
Arthur Ferreira <tutudacosta996@gmail.com>
2025-05-24 13:26:05 -0300

    testes 02

--- js/calculator.js
+++ js/calculator.js
@@ -2,13 +2,14 @@ function calc() {
     let op = prompt('Qual é operador a seguir você quer? (+/-/x/÷)')
     let title = document.getElementById('title')
     let btn = document.getElementById('btn-op')
+    let fim = document.getElementById('res')
 
     // Input
     const input1 = document.getElementById('n1')
     const input2 = document.getElementById('n2')
     // String
-    const strNumber1 = +input1.value;
-    const strNumber2 = +input2.value;
+    const strNumber1 = +input1.value
+    const strNumber2 = +input2.value
     // Conversão
     const n1 = strNumber1
     const n2 = strNumber2
@@ -20,7 +21,6 @@ function calc() {
         title.innerHTML = "Adição"
         let resul = n1 + n2
 
-        let fim = document.getElementById('res')
         fim.style.display = 'block'
 
         if (n1 == '' || n2 == '' || op == '') { // Tratamento de Error void
@@ -34,13 +34,13 @@ function calc() {
         title.innerHTML = "Subtração"
         let resul = n1 - n2
 
-        let fim = document.getElementById('res')
         fim.style.display = 'block'
         
         if (n1 == '' || n2 == '' || op == '') { // Tratamento de Error void
             fim.innerHTML = 'Não foi possível calcular &#x26D4;'
         } else {
-            fim.innerHTML = `O resultado é ${resul} &#x2728;`
+            fim.innerHTML = `O resultado é ${Math.abs(resul)} &#x2728;`
+            // 'Math.abs() é para deixar o número positivo'
         }
     } else if (op == 'x') {
         btn.style.display = 'none'
@@ -48,7 +48,6 @@ function calc() {
         title.innerHTML = "Multiplicação"
         let resul = n1 * n2
 
-        let fim = document.getElementById('res')
         fim.style.display = 'block'
 
         if (n1 == '' || n2 == '' || op == '') { // Tratamento de Error void
@@ -62,7 +61,6 @@ function calc() {
         title.innerHTML = "Divisão"
         let resul = n1 / n2
 
-        let fim = document.getElementById('res')
         fim.style.display = 'block'
 
         if (n1 == '' || n2 == '' || op == '') { // Tratamento de Error void
@@ -74,14 +72,18 @@ function calc() {
         btn.style.display = 'none'
         title.style.display = 'block'
         title.innerHTML = "Nenhum operador identificado"
+        title.style.width = '150px'
 
-        let fim = document.getElementById('res')
         fim.style.display = 'block'
 
         if (n1 == '' || n2 == '' || op == '') { // Tratamento de Error void
             fim.innerHTML = 'Não foi possível calcular &#x26D4;'
-        } else {
-            fim.innerHTML = `O resultado é ${resul} &#x2728;`
         }
     }
+}
+
+function Alert() { // Retiramento do aviso em vermelho
+    let section = document.getElementById('alert')
+    section.style.display = 'none'
+    section.style.marginTop = '0px'
 }
